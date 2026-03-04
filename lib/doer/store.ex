c3232a44a8d1ce4831efdf943066a21afe477168
armanarutiunov <arman.arutiunov@fresha.com>
2026-03-04 16:37:20 +0000

    fix store dir resolved at runtime instead of compile time

--- lib/doer/store.ex
+++ lib/doer/store.ex
@@ -2,20 +2,20 @@ defmodule Doer.Store do
   alias Doer.Todo
   alias Doer.Project
 
-  @dir Path.expand("~/.doer")
-  @legacy_path Path.join(@dir, "todos.json")
-  @all_todos_path Path.join(@dir, "all-todos.json")
-  @projects_dir Path.join(@dir, "projects")
+  defp dir, do: Path.expand("~/.doer")
+  defp legacy_path, do: Path.join(dir(), "todos.json")
+  defp all_todos_path, do: Path.join(dir(), "all-todos.json")
+  defp projects_dir, do: Path.join(dir(), "projects")
 
   def init do
-    File.mkdir_p!(@dir)
-    File.mkdir_p!(@projects_dir)
+    File.mkdir_p!(dir())
+    File.mkdir_p!(projects_dir())
     migrate()
   end
 
   defp migrate do
-    if File.exists?(@legacy_path) and not File.exists?(@all_todos_path) do
-      File.rename!(@legacy_path, @all_todos_path)
+    if File.exists?(legacy_path()) and not File.exists?(all_todos_path()) do
+      File.rename!(legacy_path(), all_todos_path())
     end
   end
 
@@ -30,7 +30,7 @@ defmodule Doer.Store do
   # All Todos (ungrouped)
 
   def load_all_todos do
-    case File.read(@all_todos_path) do
+    case File.read(all_todos_path()) do
       {:ok, contents} ->
         contents |> Jason.decode!() |> Enum.map(&Todo.from_map/1)
 
@@ -42,18 +42,18 @@ defmodule Doer.Store do
   end
 
   def save_all_todos(todos) do
-    write_json(@all_todos_path, todos)
+    write_json(all_todos_path(), todos)
   end
 
   # Projects
 
   def load_projects do
-    case File.ls(@projects_dir) do
+    case File.ls(projects_dir()) do
       {:ok, files} ->
         files
         |> Enum.filter(&String.ends_with?(&1, ".json"))
         |> Enum.map(fn file ->
-          path = Path.join(@projects_dir, file)
+          path = Path.join(projects_dir(), file)
           data = path |> File.read!() |> Jason.decode!()
           project = Project.from_map(data)
           todos = (data["todos"] || []) |> Enum.map(&Todo.from_map/1)
@@ -77,12 +77,12 @@ defmodule Doer.Store do
       "todos" => todos
     }
 
-    path = Path.join(@projects_dir, "#{project.id}.json")
+    path = Path.join(projects_dir(), "#{project.id}.json")
     write_json(path, data)
   end
 
   def delete_project(project_id) do
-    path = Path.join(@projects_dir, "#{project_id}.json")
+    path = Path.join(projects_dir(), "#{project_id}.json")
     File.rm(path)
   end
 
